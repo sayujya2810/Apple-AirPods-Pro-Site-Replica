16e6768ccddcf6de569b74e7ea4c2fdbcd860440
sayujya2810 <64795363+sayujya2810@users.noreply.github.com>
2021-01-01 12:54:48 +0530

    Add files via upload

---
+++ Video Animated Scrolling effect/app.js
@@ -0,0 +1,49 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector(".intro h1");
+//END SECTION
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 5000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
+
+  // Video Animation
+
+  let accelAmount = 0.1;
+  let scrollpos = 0;
+  let delay = 0;
+
+  scene.on("update", e => {
+      scrollpos = e.scrollPos / 1000;
+  });
+
+  setInterval(() => {
+      delay += (scrollpos - delay) * accelAmount;
+      video.currentTime = delay;
+  },33.33);
+
+// text Animation
+
+const textAnim = TweenMax.fromTo(text, 3, {opacity: 1}, {opacity: 0});
+
+
+  let scene2 =  new ScrollMagic.Scene({
+      duration: 3000,
+      triggerElement: intro,
+      triggerHook: 0
+  })
+  .setTween(textAnim)
+  .addTo(controller);
+
+ 
